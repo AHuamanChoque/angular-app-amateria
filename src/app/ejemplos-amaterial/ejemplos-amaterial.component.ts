073ec99a4a3742c4260691617f6414caf5ee4ab1
AHuamanChoque <adriansan734@gmail.com>
2024-08-08 21:13:20 -0500

    arreglando angular material

---
+++ src/app/ejemplos-amaterial/ejemplos-amaterial.component.ts
@@ -0,0 +1,54 @@
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../angular-material/material/material.module';
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  subtasks?: Task[];
+}
+
+@Component({
+  selector: 'app-ejemplos-amaterial',
+  standalone: true,
+  imports: [MaterialModule,FormsModule],
+  templateUrl: './ejemplos-amaterial.component.html',
+  styleUrl: './ejemplos-amaterial.component.css'
+})
+export class EjemplosAmaterialComponent {
+
+  longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
+  Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
+  usually kept as a companion animal or for showing.`;
+
+  readonly task = signal<Task>({
+    name: 'Parent task',
+    completed: false,
+    subtasks: [
+      {name: 'Child task 1', completed: false},
+      {name: 'Child task 2', completed: false},
+      {name: 'Child task 3', completed: false},
+    ],
+  });
+  readonly partiallyComplete = computed(() => {
+    const task = this.task();
+    if (!task.subtasks) {
+      return false;
+    }
+    return task.subtasks.some(t => t.completed) && !task.subtasks.every(t => t.completed);
+  });
+
+  update(completed: boolean, index?: number) {
+    this.task.update(task => {
+      if (index === undefined) {
+        task.completed = completed;
+        task.subtasks?.forEach(t => (t.completed = completed));
+      } else {
+        task.subtasks![index].completed = completed;
+        task.completed = task.subtasks?.every(t => t.completed) ?? true;
+      }
+      return {...task};
+    });
+  }
+
+}
